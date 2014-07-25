1e69e3818d5ec10b61fd63d5884bd7ae98c9d92c
bshteinfeld <bshteinfeld@10.1.44.185>
2014-07-25 09:03:15 -0400

    Remove unnecessary SQLException.

--- src/queuemonitor/QueueMonitor.java
+++ src/queuemonitor/QueueMonitor.java
@@ -1,14 +1,12 @@
 package queuemonitor;
 
-import java.sql.*;
-
 /**
  *
  * @author bshteinfeld
  */
 public class QueueMonitor {
     
-    public static void main(String[] args) throws SQLException{      
+    public static void main(String[] args) {      
         QMController controller = new QMController();
     }
 }
